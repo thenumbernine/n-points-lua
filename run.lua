9440410e8e9635419e970251e41621faae693181
Christopher Moore <christopher.e.moore@gmail.com>
2023-12-06 18:07:27 +0700

    made it more stable

--- run.lua
+++ run.lua
@@ -13,9 +13,12 @@ numPoints = 4
 dt = .1
 
 function reset()
-	pts = range(numPoints):map(function(i)
+	pts = range(numPoints):mapi(function(i)
 		if i == 1 then return vec3d(1,0,0) end
 		return (vec3d(math.random(), math.random(), math.random())*.2-vec3d(1,1,1)):normalize()
+	end)
+	vels = range(numPoints):mapi(function(i)
+		return vec3d()
 	end)
 end
 
@@ -35,21 +38,36 @@ end
 function App:update()
 	gl.glClear(bit.bor(gl.GL_COLOR_BUFFER_BIT, gl.GL_DEPTH_BUFFER_BIT))
 
+	gl.glColor3f(.5, .5, .5)
+	gl.glBegin(gl.GL_LINES)
+	for i=1,#pts-1 do
+		for j=i+1,#pts do
+			gl.glVertex3d(pts[i]:unpack())
+			gl.glVertex3d(pts[j]:unpack())
+		end
+	end
+	gl.glEnd()
 	gl.glPointSize(3)
+	gl.glColor3f(1, 1, 1)
 	gl.glBegin(gl.GL_POINTS)
 	for _,p in ipairs(pts) do
 		gl.glVertex3d(p:unpack())
 	end
 	gl.glEnd()
 
+	-- accum vels before normalize = converges without constantly spinning
 	for i=1,#pts do
-		for j=1,#pts do
-			if i ~= j then
-				local d = (pts[i] - pts[j]):normalize()
-				pts[i] = (pts[i] + d * dt):normalize()
-				pts[j] = (pts[j] - d * dt):normalize()
-			end
+		vels[i]:set(0,0,0)
+	end
+	for i=1,#pts-1 do
+		for j=i+1,#pts do
+			local d = (pts[i] - pts[j]):normalize()
+			vels[i] = vels[i] + d
+			vels[j] = vels[j] - d
 		end
+	end
+	for i=1,#pts do
+		pts[i] = (pts[i] + vels[i] * dt):normalize()
 	end
 
 	App.super.update(self)
